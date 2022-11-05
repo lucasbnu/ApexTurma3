ad174bade4bf196c124fff9c0e2a93d44486ff61
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-11-05 11:16:05 -0300

    Apagar e alterar

--- JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/controle/ClientesControle.java
+++ JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/controle/ClientesControle.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import Apex.API.Mercado.modelo.ClientesModelo;
@@ -37,5 +39,23 @@ public class ClientesControle {
     public void salvar(@RequestBody ClientesModelo cli) {
     	rep.save(cli);
     }
+    
+    @DeleteMapping(path = "deletar/{codigo}")
+    public void deletar(@PathVariable("codigo") Integer codigo ) {
+    	try {
+		  rep.deleteById(codigo);	
+		} catch (Exception e) {
+		      System.out.print(e.getMessage());
+		}
+    }
+    
+    @RequestMapping(path = "altera", method = RequestMethod.PUT)
+    public void altera(@RequestBody ClientesModelo cli) {
+    	try {
+	       rep.save(cli);
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+    }    
 	
 }
