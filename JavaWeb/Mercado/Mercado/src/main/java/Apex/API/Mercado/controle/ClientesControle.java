4f75666d20a892e3d5f348e1caa8f80a37b51c82
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-11-05 10:03:07 -0300

    Implementando get all

---
+++ JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/controle/ClientesControle.java
@@ -0,0 +1,24 @@
+package Apex.API.Mercado.controle;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Apex.API.Mercado.modelo.ClientesModelo;
+import Apex.API.Mercado.repositorio.ClientesRepositorio;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClientesControle {
+	@Autowired
+	ClientesRepositorio rep;
+	
+	@GetMapping(path = "buscar")
+	public List<ClientesModelo> buscar(){
+		return rep.findAll();
+	}
+
+}
