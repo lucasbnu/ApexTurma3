6bf1d67d374d70ec84ff98801eca16e1d2c1fd33
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-11-05 09:31:27 -0300

    Atualizando modelo

--- JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/modelo/ClientesModelo.java
+++ JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/modelo/ClientesModelo.java
@@ -1,5 +1,58 @@
 package Apex.API.Mercado.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "clientes")
 public class ClientesModelo {
+	
+   public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNome_cliente() {
+		return nome_cliente;
+	}
+
+	public void setNome_cliente(String nome_cliente) {
+		this.nome_cliente = nome_cliente;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(int ativo) {
+		this.ativo = ativo;
+	}
+
+@Id	
+   @GeneratedValue(strategy = GenerationType.IDENTITY )
+   @Column(name="id_cliente")
+   private int id_cliente;	
+   
+	@Column(name="nome_cliente")
+	private String nome_cliente;   
+	
+	@Column(name = "idade")
+	private int idade;
+	
+	@Column(name = "ativo")
+	private int ativo;	
 
 }
