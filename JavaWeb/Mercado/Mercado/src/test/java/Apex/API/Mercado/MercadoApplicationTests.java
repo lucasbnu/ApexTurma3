14e61a7ecd14162b73afc771150168414b586b85
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-11-05 08:58:15 -0300

    Iniciando projeto api

---
+++ JavaWeb/Mercado/Mercado/src/test/java/Apex/API/Mercado/MercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package Apex.API.Mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
