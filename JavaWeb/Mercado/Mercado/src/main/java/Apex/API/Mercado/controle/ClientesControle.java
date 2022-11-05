846ea138c903ce3126d97b5a941712e05a494615
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-11-05 11:14:40 -0300

    Metodo de salvar

--- JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/controle/ClientesControle.java
+++ JavaWeb/Mercado/Mercado/src/main/java/Apex/API/Mercado/controle/ClientesControle.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,10 @@ public class ClientesControle {
 		Optional<ClientesModelo> cliente = rep.findById(codigo);
 		return cliente.get();
     }	
+    
+    @PostMapping(path = "salvar")
+    public void salvar(@RequestBody ClientesModelo cli) {
+    	rep.save(cli);
+    }
 	
 }
